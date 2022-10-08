f79d7de23b2d087e972364f02c3bfa87adeeda03
Abdullah Darwish <Abdullah.Darwish@syngenta.com>
2022-10-08 22:32:54 +0200

    fixed hpa

--- set_env.sh
+++ set_env.sh
@@ -2,7 +2,7 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=Abc123456
-export POSTGRES_HOST=my-udacity-project-03.cxwiqhhqw7fr.us-east-2.rds.amazonaws.com
+export POSTGRES_HOST=database-1.cfmj2697hyjc.us-east-2.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=cloud-developer-project-03
 export AWS_REGION=us-east-2
